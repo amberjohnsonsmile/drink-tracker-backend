aa318217d7a19f5482906d73815bc041fcc5a342
Amber Johnson <amber.johnson.smile@gmail.com>
2018-03-24 17:30:15 -0600

    Add dateString

--- seeds/01_drink.js
+++ seeds/01_drink.js
@@ -6,16 +6,19 @@ exports.seed = (knex, Promise) => {
         {
           id: 1,
           date: '2018-03-01',
+          dateString: 'Thursday, March 1',
           drinks: 3
         },
         {
           id: 2,
           date: '2018-03-02',
+          dateString: 'Friday, March 2',
           drinks: 0
         },
         {
           id: 3,
           date: '2018-03-03',
+          dateString: 'Saturday, March 3',
           drinks: 0
         }
       ]);
